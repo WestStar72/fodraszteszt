a8f56e6f54f0c88499fe2a90293a397c1221386a
Hajdler Bence <107484339+WestStar72@users.noreply.github.com>
2025-06-20 18:13:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxHp18ri8svv5gMbsmDdBLTr5q68JVgwr-lh5TIYV2klYUDi6YjKyVOSZGoRpvSaOIV/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyYEUINy4MNI0p6_ovnGpYvXLvkqkdi5XrVswg-a6Es69QzLfjaDOA9boJa_ctINa85/exec";
 const slotsContainer = document.getElementById("slots");
 const form = document.getElementById("bookingForm");
 const nameInput = document.getElementById("name");
